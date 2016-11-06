5ff321181136a715e4ced6b975164821a587e429
Kevin Modzelewski <kevmod@gmail.com>
2016-11-06 01:06:48 -0800

    continuing tutorial

--- django/paper_tracker/paper_tracker/settings.py
+++ django/paper_tracker/paper_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'papers.apps.PapersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
