1bc3b0943c8a5753f4222cd3eee55953c8a04a46
Kevin Modzelewski <kevmod@gmail.com>
2016-11-06 18:46:02 -0800

    manytomany

--- django/paper_tracker/papers/models.py
+++ django/paper_tracker/papers/models.py
@@ -9,9 +9,16 @@ class Paper(models.Model):
 
 class Collection(models.Model):
     name = models.CharField(max_length=255)
-    papers = models.ManyToManyField(Paper)
+    papers = models.ManyToManyField(Paper, through='CollectionPapers')
 
     def __str__(self):
         # return "Collection %d: %s" % (self.pk, self.name)
         return "Collection %d: %s, with %d papers" % (self.pk, self.name, len(self.papers.all()))
 
+class CollectionPapers(models.Model):
+    paper = models.ForeignKey(Paper)
+    collection = models.ForeignKey(Collection)
+
+    priority = models.IntegerField(default=1)
+    read = models.IntegerField(default=1)
+    notes = models.CharField(max_length=255)
