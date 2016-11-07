0734444af162cd3468eaa92e8a10d233a0e33325
Kevin Modzelewski <kevmod@gmail.com>
2016-11-06 22:21:26 -0800

    mvp

--- django/paper_tracker/papers/urls.py
+++ django/paper_tracker/papers/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     url(r'^papers$', views.papers_index, name='papers_index'),
     url(r'^collections$', views.collections_index, name='collections_index'),
     url(r'^collection/(?P<collection_id>[0-9]+)/$', views.collection, name='collection'),
+    url(r'^paper/new$', views.paper_new, name='paper_new'),
+    # url(r'^paper/(?P<paper_id>[0-9]+)$', views.paper, name='paper'),
+    url(r'^paper/(?P<paper_id>[0-9]+)/find_pdf$', views.paper_findpdf, name='paper_findpdf'),
+    url(r'^paper/(?P<paper_id>[0-9]+)/delete$', views.paper_delete, name='paper_delete'),
+    url(r'^collection/(?P<collection_id>[0-9]+)/edit/(?P<paper_id>[0-9]+)$', views.cpaper, name='cpaper'),
 ]
--- django/paper_tracker/papers/views.py
+++ django/paper_tracker/papers/views.py
@@ -1,11 +1,62 @@
-from django.shortcuts import render, get_object_or_404
+import urllib
+
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import HttpResponse
 
-from .models import Paper, Collection
+from .models import Paper, Collection, CollectionPapers
+
+def cpaper(request, collection_id, paper_id):
+    memb = get_object_or_404(CollectionPapers, collection_id=collection_id, paper_id=paper_id)
+
+    if request.method == "POST":
+        memb.paper.title = request.POST['title']
+        memb.paper.pdf_url = request.POST['pdf_url']
+        memb.notes = request.POST['notes']
+        memb.save()
+        memb.paper.save()
+        return redirect(urllib.parse.unquote(request.GET['back']))
+
+    context = {
+        'memb': memb,
+        'back_url': request.GET['back'],
+    }
+    return render(request, "papers/cpaper.html", context)
+
 
 def papers_index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    papers = Paper.objects.all()
+    context = {
+        'papers': papers,
+    }
+    return render(request, "papers/papers.html", context)
+
+def paper_delete(request, paper_id):
+    p = get_object_or_404(Paper, pk=paper_id)
+    if request.method == "POST":
+        p.delete()
+        return redirect(urllib.parse.unquote(request.GET['back']))
+
+    p = get_object_or_404(Paper, pk=paper_id)
+    context = {
+        'back_url': request.GET['back'],
+    }
+    return render(request, "papers/paper_delete.html", context)
+
+
+def paper_findpdf(request, paper_id):
+    p = get_object_or_404(Paper, pk=paper_id)
+    if request.method == "POST":
+        url = request.POST['url']
+        p.pdf_url = url
+        p.save()
+        return redirect(urllib.parse.unquote(request.GET['back']))
+
+    context = {
+        'paper': p,
+        'search_query': urllib.parse.quote(p.title),
+    }
+    return render(request, "papers/paper_findpdf.html", context)
 
 def collections_index(request):
     collections = Collection.objects.all()
@@ -18,5 +69,26 @@ def collection(request, collection_id):
     c = get_object_or_404(Collection, pk=collection_id)
     context = {
         'collection': c,
+        'back_url': urllib.parse.quote(request.get_full_path()),
     }
     return render(request, "papers/collection.html", context)
+
+def paper_new(request):
+    if request.method == "POST":
+        assert request.POST["Title"]
+        p = Paper(title=request.POST['Title'])
+        p.save()
+
+        add_to = int(request.GET['add_to'])
+        print(Collection.objects.get(pk=add_to).collectionpapers_set)
+        p.collectionpapers_set.create(collection_id=int(request.GET['add_to']))
+        print(Collection.objects.get(pk=add_to).collectionpapers_set)
+        p.save()
+        print(Collection.objects.get(pk=add_to).collectionpapers_set)
+
+        return redirect("/collection/%s" % request.GET['add_to'])
+
+    context = {
+    }
+    return render(request, "papers/paper_new.html", context)
+
